19ec01cf652438fc64b4af05eae7535b4b018afc
joel <whocares>
2015-07-17 22:07:04 +1000

    MovedFiles

---
+++ play/battleField.cpp
@@ -0,0 +1,111 @@
+#include "battleField.h"
+#include "../play/mob.h"
+#include "../play/gameMap.h"
+
+using namespace Magic;
+
+BattleField::BattleField(GameMap* map_)
+{
+    _pcs.push_back(map_->pc());
+
+    for(int i = 1; i < int(map_->mobs().size()); i++)
+    {
+        if (map_->mobs().at(i)->isSeen(map_->pc()))
+            _hostiles.push_back(map_->mobs().at(i));
+    }
+}
+
+/**
+ * Return true if a victory condition (for the player) has been met.
+ */
+bool BattleField::isVictory(void) const
+{
+    for (Mob* m : _hostiles)
+    {
+        if (m->stamina() > 0)
+            return false;
+    }
+
+    return true;
+}
+
+/**
+ * Return true if a game over condition has been met.
+ */
+bool BattleField::isDefeat(void) const
+{
+    for (Mob* m : _pcs)
+    {
+        if (m->stamina() > 0)
+            return false;
+    }
+
+    return true;
+}
+
+/**
+ *
+ */
+std::vector<Mob*> BattleField::getDue(void)
+{
+    std::vector<Mob*> result = std::vector<Mob*>(0);
+    if (!_isInCombat)
+        return result;
+
+    int time = SDL_GetTicks();
+    for (Mob* item : mobs())
+    {
+        // Now is the time to clean up dead mobs.
+        if (item->stamina() <= 0)
+        {
+            for (int i = 0; i < int(_hostiles.size()); i++)
+                if (item == _hostiles.at(i))
+                    _hostiles.erase(_hostiles.begin() + i);
+            for (int i = 0; i < int(_pcs.size()); i++)
+                if (item == _pcs.at(i))
+                    _pcs.erase(_pcs.begin() + i);
+            continue;
+        }
+
+        if (item->tryUnblock(time) || !item->isBlocked())
+            result.push_back(item);
+    }
+    result.shrink_to_fit();
+    return result;
+}
+
+bool BattleField::areAllied(const MapObject* one, const MapObject* other) const
+{
+    Mob* mOne = (Mob*) one;
+    Mob* mOther = (Mob*) other;
+    return mOne->type() == mOther->type();
+}
+
+bool BattleField::isInCombat(bool isInCombat_)
+{
+    _isInCombat = isInCombat_;
+    return _isInCombat;
+}
+bool BattleField::isInCombat(void) const
+{
+    return _isInCombat;
+}
+
+std::vector<Mob*> BattleField::hostiles(void)
+{
+    return _hostiles;
+}
+
+std::vector<Mob*> BattleField::pcs(void)
+{
+    return _pcs;
+}
+
+std::vector<Mob*> BattleField::mobs(void)
+{
+    std::vector<Mob*> result = std::vector<Mob*>();
+    result.insert(result.end(), _hostiles.begin(), _hostiles.end());
+    result.insert(result.end(), _pcs.begin(), _pcs.end());
+    return result;
+}
+
---
+++ play/combatManager.cpp
@@ -0,0 +1,185 @@
+#include "combatManager.h"
+
+using namespace Magic;
+
+CombatManager::CombatManager(SDL_Renderer* r, AssetCache* a, ScreenViewContainer views) : StateManager(r, a)
+{
+    _controlView = (ControlViewManager*) views.Top;
+    _statsView = (StatsViewManager*) views.Right;
+    _miniMapView = (MiniMapViewManager*) views.TopRight;
+    _mapView = (MapViewManager*) views.Main;
+}
+
+CombatManager::~CombatManager(void) {};
+
+Play::PlayState CombatManager::start(void)
+{
+    return Play::PlayState::Exit;
+}
+
+Play::PlayState CombatManager::start(GameMap* map_)
+{
+    state(Play::PlayState::Movement);
+
+    _map = map_;
+    SDL_Event event;
+    BattleField field = BattleField(map_);
+    _field = &field;
+
+    field.isInCombat(true);
+    bool rerender = true;
+
+    while(field.isInCombat())
+    {
+        _map->buryTheDead();
+
+        if (rerender)
+            render();
+
+        rerender = false;
+
+
+        std::vector<SDL_Event> events;
+        while(SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+                return Play::PlayState::Exit;
+            events.push_back(event);
+        }
+
+        std::vector<Mob*> due = field.getDue();
+        for(Mob* combatant : due)
+        {
+            if (combatant->type() == MobType::PlayerCharacter)
+                rerender = processPcTurn(combatant, &field, &events);
+            else if (combatant->type() == MobType::Hostile)
+                rerender = processHostileTurn((Enemy*)combatant, &field);
+        }
+
+        if (field.isDefeat())
+            state(Play::PlayState::GameOver);
+    }
+
+    for (Mob* mob : field.mobs())
+    {
+        mob->endCombat();
+    }
+
+    _field = nullptr;
+    _map = nullptr;
+    return result();
+}
+
+void CombatManager::render(void)
+{
+    SDL_SetRenderDrawColor(renderer(), 0x00, 0x00, 0x00, 0xFF);
+    SDL_RenderClear(renderer());
+
+    Play::PlayState state = Play::PlayState::Combat;
+    _mapView->render(_map, state);
+    _controlView->render(_map->pc(), state);
+    _statsView->render(_map, state);
+    _miniMapView->render();
+
+    SDL_RenderPresent(renderer());
+}
+
+bool CombatManager::moveCursor(Mob* mob, Core::InputPress input)
+{
+    int index = mob->selectedCommandIndex();
+    int noOfCommands= mob->commands().size();
+    int newIndex = StateManager::moveCursor(input, index, noOfCommands, _controlView->menuItemsPerColumn());
+
+    if (index == newIndex)
+        return false;
+
+    return (mob->selectedCommandIndex(newIndex) != -1);
+}
+
+bool CombatManager::processHostileTurn(Enemy* mob, BattleField* field)
+{
+    mob->aiAct(field);
+    return true;
+}
+
+bool CombatManager::processPcTurn(Mob* pc, BattleField* field, std::vector<SDL_Event>* events)
+{
+    bool hasUpdate;
+
+    // If there
+    if (events->size() <= 0)
+        return true;
+    while(events->size() > 0)
+    {
+        SDL_Event event = events->back();
+        events->pop_back();
+        if (event.type != SDL_KEYDOWN)
+            continue;
+
+        switch(event.key.keysym.sym)
+        {
+            case SDLK_w:
+                hasUpdate |= moveCursor(pc, Core::InputPress::UP);
+                break;
+            case SDLK_a:
+                hasUpdate |= moveCursor(pc, Core::InputPress::LEFT);
+                break;
+            case SDLK_s:
+                hasUpdate |= moveCursor(pc, Core::InputPress::DOWN);
+                break;
+            case SDLK_d:
+                hasUpdate |= moveCursor(pc, Core::InputPress::RIGHT);
+                break;
+            case SDLK_RIGHTBRACKET:
+                hasUpdate |= processCommand(pc, field);
+                break;
+            case SDLK_RETURN:
+                // Pause
+                break;
+        }
+    }
+
+    return hasUpdate;
+}
+
+bool CombatManager::processCommand(Mob* mob, BattleField* field)
+{
+    // Carry out the command
+    int waitMs = mob->selectedCommand()->execute(mob, field);
+
+    // Apply the duration - mob cannot act again until command completes.
+    mob->block(SDL_GetTicks() + waitMs);
+
+    if (field->isDefeat())
+    {
+        state(Play::PlayState::GameOver);
+        return true;
+    }
+    if (field->isVictory())
+        state(Play::PlayState::Victory);
+
+    return true;
+}
+
+Play::PlayState CombatManager::result(void) const
+{
+    return StateManager::state();
+}
+
+Play::PlayState CombatManager::state(Play::PlayState state_)
+{
+    StateManager::state(state_);
+    if (_field != nullptr)
+        switch(state_)
+        {
+            case Play::PlayState::GameOver:
+            case Play::PlayState::Victory:
+                _field->isInCombat(false);
+                break;
+            default: _field->isInCombat(true);
+                break;
+        }
+    return StateManager::state();
+}
+
+
